8f83d0266bf465cd44328320997f4544e16a7d6e
Diego <dicornyt@gmail.com>
2023-01-26 10:31:09 -0500

    feat: icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,10 @@ function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
 
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" href="/pages/home/logo2.png" />
+      </Head>
+
       {<Component {...pageProps} />}
     </>
   )
