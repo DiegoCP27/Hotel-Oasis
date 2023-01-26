55d78ac6c9446066ad4e09c867a9ac17b40d6869
Diego <dicornyt@gmail.com>
2023-01-25 23:26:20 -0500

    refactor: eliminar componente

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Links } from './links/Links.component';
 export { default as MenuNavbarDesktop } from './menu-navbar-desktop/MenuNavbarDesktop.component';
 export { default as MenuNavbarMobile } from './menu-navbar-mobile/MenuNavbarMobile.component';
 export { default as Lightbox } from './lightbox/Lightbox.component';
