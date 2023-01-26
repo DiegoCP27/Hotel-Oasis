061ae311c1c29e48cf90132aeb75944e5b019636
Diego <dicornyt@gmail.com>
2023-01-25 23:29:05 -0500

    feat: data

---
+++ src/data/navbar-data/Navbar.data.ts
@@ -0,0 +1,24 @@
+import { ItemNavbarProps } from '@/models'
+
+export const DataContactoCorporativos: ItemNavbarProps[] = [
+  {
+    id: 1,
+    url: '#home',
+    name: 'Home'
+  },
+  {
+    id: 2,
+    url: '#nosotros',
+    name: 'Nosotros'
+  },
+  {
+    id: 3,
+    url: '#habitaciones',
+    name: 'Habitaciones'
+  },
+  {
+    id: 4,
+    url: '#contacto',
+    name: 'Contacto'
+  }
+]
